3e533dc6dd0a0c18d7b25835e810b261fc6db24b
maninder singh <nsit.maninder@gmail.com>
2019-08-05 21:00:42 +0530

    add README.md file and remove example from main code

--- README.md
+++ README.md
@@ -1,3 +1,75 @@
 # async-array
 
-A utility for managing array operation
+A utility for managing array operation on asynchronous data.
+`filter`,`map` and `forEach` implementation of `Array` to work like a synchronous wise.
+
+### Import 
+script tag to load file in browser
+```
+<script
+  src="./async-array.js"
+  type="application/javascript"
+  crossorigin="anonymous"></script>
+```
+
+### Examples 
+```
+new AsyncArray([1, 2, 3, 4, 5])
+    .forEach(num => {
+        return delay().then(() => {
+            console.log(`first => ${num}`);
+        })
+    })
+    .filter(num => {
+        return delay().then(() => {
+            return num >= 3;
+        })
+    })
+    .forEach(num => {
+        return delay().then(() => {
+            console.log(`second => ${num}`);
+        })
+    })
+    .filter(num => {
+        return delay().then(() => {
+            return num >= 4;
+        })
+    })
+    .map(num => {
+        return delay().then(() => {
+            return num + 2;
+        })
+    })
+    .forEach(num => {
+        return delay().then(() => {
+            console.log(`third => ${num}`);
+        })
+    })
+    .forEach(num => {
+        return delay().then(() => {
+            console.log(`fourth => ${num}`);
+        })
+    });
+
+function delay(ms = 200) {
+    return new Promise(function(resolve){
+        setTimeout(resolve, ms);
+    });
+}
+
+// Output 
+
+first => 1
+first => 2
+first => 3
+first => 4
+first => 5
+second => 3
+second => 4
+second => 5
+third => 6
+third => 7
+fourth => 6
+fourth => 7
+
+```
--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-(function () {
-    function promisify() {
+var AsyncArray = (function(){
+    function promisify(){
         function promise(fn, method) {
             new Promise((resolve, reject) => {
                 Promise.all(this._data.map(fn)).then(values => {
@@ -19,20 +19,23 @@
         singleMethodCall.call(this);
     }
 
-    function singleMethodCall() {
+    function singleMethodCall(){
         if (this.chains.length === 1) {
             this.chains[0].call();
         }
     }
 
     function AsyncArray(data) {
+        if(!(this instanceof AsyncArray)){
+            return new AsyncArray(data);
+        }
         this._data = data;
         this.chains = [];
     }
 
     AsyncArray.prototype.filter = function (fn) {
-        promisify.call(this, fn, (input, values) => {
-            return input.filter((value, index) => {
+        promisify.call(this, fn,function (input, values){
+            return input.filter(function (value, index){
                 return values[index];
             });
         });
@@ -40,56 +43,20 @@
     };
 
     AsyncArray.prototype.map = function (fn) {
-        promisify.call(this, fn, (input, values) => {
-            return input.map((value, index) => {
+        promisify.call(this, fn,function(input, values){
+            return input.map(function (value, index){
                 return values[index];
             });
         });
         return this;
     };
 
-    AsyncArray.prototype.each = function (fn) {
-        promisify.call(this, fn, (input) => {
+    AsyncArray.prototype.forEach = function (fn) {
+        promisify.call(this, fn,function (input){
             return input;
         });
         return this;
     };
-    window.AsyncArray = AsyncArray;
 
-    // example
-    new AsyncArray([1, 2, 3, 4, 5]).each(num => {
-        return delay().then(() => {
-            console.log(`first => ${num}`);
-        })
-    })
-        .filter(num => {
-            return delay().then(() => {
-                return num >= 3;
-            })
-        }).each(num => {
-        return delay().then(() => {
-            console.log(`second => ${num}`);
-        })
-    }).filter(num => {
-        return delay().then(() => {
-            return num >= 4;
-        })
-    })
-        .map(num => {
-            return delay().then(() => {
-                return num + 2;
-            })
-        }).each(num => {
-        return delay().then(() => {
-            console.log(`fifth => ${num}`);
-        })
-    }).each(num => {
-        return delay().then(() => {
-            console.log(`sixth => ${num}`);
-        })
-    });
-
-    function delay(ms = 200) {
-        return new Promise(resolve => setTimeout(resolve, ms));
-    }
+    return AsyncArray;
 })();
